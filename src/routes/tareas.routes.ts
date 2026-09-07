17f9d3db6f01046965af1fb6fc3b91695ba45f21
Bryan Jesus <bryan2409021@hybridge.education>
2026-09-07 02:36:23 -0600

    Actividad completada correctamente

---
+++ src/routes/tareas.routes.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+
+import { obtenerTareas } from '../controllers/tareas.controller';
+
+const router = express.Router();
+
+router.get('/tareas', obtenerTareas);
+
+export default router;
